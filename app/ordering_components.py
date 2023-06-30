155ba5f006846edbf2649874cbb7236e1830ea86
Max Wang <max@covar.com>
2023-06-30 13:06:15 -0400

    Add gets for safety

--- app/ordering_components.py
+++ app/ordering_components.py
@@ -5,7 +5,7 @@ def get_confidence(result, message, logger):
     # TODO Implement actual g()-score
     score_sum = 0
     score_count = 0
-    for analysis in result["analyses"]:
+    for analysis in result.get("analyses") or []:
         if analysis.get("score") is not None:
             score_sum += analysis["score"]
             score_count += 1
@@ -25,7 +25,7 @@ def get_novelty(result, message, logger):
                 
 def get_ordering_components(message, logger):
     logger.debug(f"Computing scores for {len(message['results'])} results")
-    for result in message["results"]:
+    for result in message.get("results") or []:
         result["ordering_components"] = {
             "confidence": get_confidence(result, message, logger),
             "clinical_evidence": get_clinical_evidence(result, message, logger),
